fada7b427919337d631f70ba9a7073e87ebe8cdc
Jimmy Cuadra <jimmy@jimmycuadra.com>
2016-03-29 02:00:43 -0700

    Implement std::error::Error for etcd::{ApiError, Error}.

--- src/error_gen.rs
+++ src/error_gen.rs
@@ -1,4 +1,6 @@
 use std::convert::From;
+use std::error::Error as StdError;
+use std::fmt::{Display, Error as FmtError, Formatter};
 use std::io::Error as IoError;
 
 use hyper::Error as HttpError;
@@ -11,13 +13,13 @@ pub enum Error {
     Api(ApiError),
     /// An HTTP error from attempting to connect to etcd.
     Http(HttpError),
-    /// An IO error, which can happen when reading the HTTP response.
-    Io(IoError),
     /// An error returned when invalid conditions have been provided for a compare-and-delete or
     /// compare-and-swap operation.
     InvalidConditions(&'static str),
     /// An error if an etcd cluster member's endpoint is not a valid URL.
     InvalidUrl(ParseError),
+    /// An IO error, which can happen when reading the HTTP response.
+    Io(IoError),
     /// An error when attempting to create a client without at least one member endpoint.
     NoEndpoints,
 }
@@ -39,6 +41,30 @@ pub struct ApiError {
 /// The result type returned by `Client` methods.
 pub type EtcdResult<T> = Result<T, Error>;
 
+impl Display for Error {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FmtError> {
+        match *self {
+            Error::Api(ref error) => write!(f, "{}", error),
+            Error::InvalidConditions(reason) => write!(f, "{}", reason),
+            Error::NoEndpoints => write!(f, "At least one endpoint is required to create a Client"),
+            ref error => write!(f, "{}", error),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn description(&self) -> &str {
+        match *self {
+            Error::Api(_) => "the etcd server returned an error",
+            Error::Http(_) => "an error occurred during the HTTP request",
+            Error::InvalidConditions(conditions) => conditions,
+            Error::InvalidUrl(_) => "a supplied endpoint could not be parsed as a URL" ,
+            Error::Io(_) => "an error occurred trying to read etcd's HTTP response",
+            Error::NoEndpoints => "at least one endpoint is required to create a Client",
+        }
+    }
+}
+
 impl From<HttpError> for Error {
     fn from(error: HttpError) -> Error {
         Error::Http(error)
@@ -54,5 +80,18 @@ impl From<IoError> for Error {
 impl From<ParseError> for Error {
     fn from(error: ParseError) -> Error {
         Error::InvalidUrl(error)
+    }
+}
+
+
+impl Display for ApiError {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), FmtError> {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl StdError for ApiError {
+    fn description(&self) -> &str {
+        &self.message
     }
 }
