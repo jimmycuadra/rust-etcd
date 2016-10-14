2675124589fa515da524666c1d9e743f5a1246fd
arthurprs <arthurprs@gmail.com>
2016-10-15 00:22:20 +0200

    Update to serde 0.8

--- src/error.rs
+++ src/error.rs
@@ -1,7 +1,7 @@
 //! Contains etcd error types.
 
-#[cfg(feature = "serde_macros")]
+#[cfg(feature = "serde_derive")]
 include!("error_gen.rs");
 
-#[cfg(not(feature = "serde_macros"))]
+#[cfg(not(feature = "serde_derive"))]
 include!(concat!(env!("OUT_DIR"), "/error.rs"));
