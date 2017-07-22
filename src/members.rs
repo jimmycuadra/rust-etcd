a95ca6f586fadffb58067114ac29faae9b04d9da
Jimmy Cuadra <jimmy@jimmycuadra.com>
2017-07-22 15:42:43 -0700

    Add members::delete_member endpoint.

--- src/members.rs
+++ src/members.rs
@@ -98,6 +98,54 @@ where
     Box::new(result)
 }
 
+/// Delete a member from the cluster.
+///
+/// # Parameters
+///
+/// * client: A `Client` to use to make the API call.
+/// * id: The unique identifier of the member to delete.
+pub fn delete_member<C>(
+    client: &Client<C>,
+    id: String,
+) -> Box<Future<Item = Response<()>, Error = Vec<Error>>>
+where
+    C: Clone + Connect,
+{
+    let http_client = client.http_client().clone();
+
+    let result = first_ok(client.endpoints().to_vec(), move |member| {
+        let url = build_url(member, &format!("/{}", id));
+        let uri = Uri::from_str(url.as_str())
+            .map_err(Error::from)
+            .into_future();
+
+        let http_client = http_client.clone();
+
+        let response = uri.and_then(move |uri| http_client.delete(uri).map_err(Error::from));
+
+        let result = response.and_then(|response| {
+            let status = response.status();
+            let cluster_info = ClusterInfo::from(response.headers());
+            let body = response.body().concat2().map_err(Error::from);
+
+            body.and_then(move |ref body| if status == StatusCode::NoContent {
+                Ok(Response {
+                    data: (),
+                    cluster_info,
+                })
+            } else {
+                match serde_json::from_slice::<ApiError>(body) {
+                    Ok(error) => Err(Error::Api(error)),
+                    Err(error) => Err(Error::Serialization(error)),
+                }
+            })
+        });
+
+        Box::new(result)
+    });
+
+    Box::new(result)
+}
 /// Lists the members of the cluster.
 ///
 /// # Parameters
