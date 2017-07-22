7c438fb0b431d1cb7f98a893fe935331a323416d
Jimmy Cuadra <jimmy@jimmycuadra.com>
2017-07-22 15:36:29 -0700

    Add members::add_member endpoint.

--- src/members.rs
+++ src/members.rs
@@ -28,10 +28,74 @@ pub struct Member {
     pub client_urls: Vec<String>,
 }
 
+/// The request body for `POST /v2/members`.
+#[derive(Debug, Serialize)]
+struct AddMember {
+    #[serde(rename = "peerURLs")]
+    pub peer_urls: Vec<String>,
+}
+
 /// A small wrapper around `Member` to match the response of `GET /v2/members`.
 #[derive(Debug, Deserialize)]
 struct ListResponse {
     members: Vec<Member>,
+}
+
+/// Add a new member to the cluster.
+///
+/// # Parameters
+///
+/// * client: A `Client` to use to make the API call.
+/// * peer_urls: URLs exposing this cluster member's peer API.
+pub fn add_member<C>(
+    client: &Client<C>,
+    peer_urls: Vec<String>,
+) -> Box<Future<Item = Response<()>, Error = Vec<Error>>>
+where
+    C: Clone + Connect,
+{
+    let add_member = AddMember { peer_urls };
+
+    let body = match serde_json::to_string(&add_member) {
+        Ok(body) => body,
+        Err(error) => return Box::new(Err(vec![Error::Serialization(error)]).into_future()),
+    };
+
+    let http_client = client.http_client().clone();
+
+    let result = first_ok(client.endpoints().to_vec(), move |member| {
+        let url = build_url(member, "");
+        let uri = Uri::from_str(url.as_str())
+            .map_err(Error::from)
+            .into_future();
+
+        let body = body.clone();
+        let http_client = http_client.clone();
+
+        let response = uri.and_then(move |uri| http_client.post(uri, body).map_err(Error::from));
+
+        let result = response.and_then(|response| {
+            let status = response.status();
+            let cluster_info = ClusterInfo::from(response.headers());
+            let body = response.body().concat2().map_err(Error::from);
+
+            body.and_then(move |ref body| if status == StatusCode::Created {
+                Ok(Response {
+                    data: (),
+                    cluster_info,
+                })
+            } else {
+                match serde_json::from_slice::<ApiError>(body) {
+                    Ok(error) => Err(Error::Api(error)),
+                    Err(error) => Err(Error::Serialization(error)),
+                }
+            })
+        });
+
+        Box::new(result)
+    });
+
+    Box::new(result)
 }
 
 /// Lists the members of the cluster.
@@ -62,7 +126,10 @@ where
 
             body.and_then(move |ref body| if status == StatusCode::Ok {
                 match serde_json::from_slice::<ListResponse>(body) {
-                    Ok(data) => Ok(Response { data: data.members, cluster_info }),
+                    Ok(data) => Ok(Response {
+                        data: data.members,
+                        cluster_info,
+                    }),
                     Err(error) => Err(Error::Serialization(error)),
                 }
             } else {
