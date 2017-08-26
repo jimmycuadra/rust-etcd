ee15d32160de90a975288bc6c52392f65fb82f9f
Jimmy Cuadra <jimmy@jimmycuadra.com>
2017-08-26 16:36:37 -0700

    Add auth::{create_user, update_user}.

--- src/auth.rs
+++ src/auth.rs
@@ -368,6 +368,55 @@ impl Permission {
     }
 }
 
+/// Creates a new user.
+pub fn create_user<C>(
+    client: &Client<C>,
+    user: NewUser,
+) -> Box<Future<Item = Response<User>, Error = Vec<Error>>>
+where
+    C: Clone + Connect,
+{
+    let http_client = client.http_client().clone();
+
+    let result = first_ok(client.endpoints().to_vec(), move |member| {
+        let body = serde_json::to_string(&user)
+            .map_err(Error::from)
+            .into_future();
+
+        let url = build_url(member, &format!("/users/{}", user.name));
+        let uri = Uri::from_str(url.as_str())
+            .map_err(Error::from)
+            .into_future();
+
+        let params = uri.join(body);
+
+        let http_client = http_client.clone();
+
+        let response = params.and_then(move |(uri, body)| {
+            http_client.put(uri, body).map_err(Error::from)
+        });
+
+        let result = response.and_then(|response| {
+            let status = response.status();
+            let cluster_info = ClusterInfo::from(response.headers());
+            let body = response.body().concat2().map_err(Error::from);
+
+            body.and_then(move |ref body| if status == StatusCode::Ok {
+                match serde_json::from_slice::<User>(body) {
+                    Ok(data) => Ok(Response { data, cluster_info }),
+                    Err(error) => Err(Error::Serialization(error)),
+                }
+            } else {
+                Err(Error::UnexpectedStatus(status))
+            })
+        });
+
+        Box::new(result)
+    });
+
+    Box::new(result)
+}
+
 /// Attempts to disable the auth system.
 pub fn disable<C>(
     client: &Client<C>,
@@ -595,6 +644,54 @@ where
     Box::new(result)
 }
 
+/// Updates an existing user.
+pub fn update_user<C>(
+    client: &Client<C>,
+    user: UserUpdate,
+) -> Box<Future<Item = Response<User>, Error = Vec<Error>>>
+where
+    C: Clone + Connect,
+{
+    let http_client = client.http_client().clone();
+
+    let result = first_ok(client.endpoints().to_vec(), move |member| {
+        let body = serde_json::to_string(&user)
+            .map_err(Error::from)
+            .into_future();
+
+        let url = build_url(member, &format!("/users/{}", user.name));
+        let uri = Uri::from_str(url.as_str())
+            .map_err(Error::from)
+            .into_future();
+
+        let params = uri.join(body);
+
+        let http_client = http_client.clone();
+
+        let response = params.and_then(move |(uri, body)| {
+            http_client.put(uri, body).map_err(Error::from)
+        });
+
+        let result = response.and_then(|response| {
+            let status = response.status();
+            let cluster_info = ClusterInfo::from(response.headers());
+            let body = response.body().concat2().map_err(Error::from);
+
+            body.and_then(move |ref body| if status == StatusCode::Ok {
+                match serde_json::from_slice::<User>(body) {
+                    Ok(data) => Ok(Response { data, cluster_info }),
+                    Err(error) => Err(Error::Serialization(error)),
+                }
+            } else {
+                Err(Error::UnexpectedStatus(status))
+            })
+        });
+
+        Box::new(result)
+    });
+
+    Box::new(result)
+}
 /// Constructs the full URL for an API call.
 fn build_url(endpoint: &Uri, path: &str) -> String {
     let maybe_slash = if endpoint.as_ref().ends_with("/") {
